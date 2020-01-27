b55b2770b385d9fd6419167818b6241cd00b1fcb
Tywunda Shivers <tywunda.shivers@gmail.com>
2020-01-26 18:01:19 -0800

    adding modified schema.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,6 @@
-USE investInSouthBay_db;
-
-INSERT INTO contact(first_name, last_name, email, phone_number, user,)
-VALUES 
-('John', 'Doe', 'john.doe@email.com', '(123)456-7890','someuser',);
-
-INSERT INTO user(name, username) 
-VALUES 
-('Fade', 20.00),
-('Fade with Beard Maintenance', 25.00), 
-('Buzz / Lineup', 15.00), 
-('Taper', 20.00), 
-('Haircut', 20.00);
-
-INSERT INTO search_properties(name, single_family_home, condo, 
-duplex, triplex, fourplex, apartment_building)
-VALUES
-('single_family_home', 12345 Some Place )
+INSERT INTO contact (first_name) VALUES ('Jill');
+INSERT INTO contact (last_name) VALUES ('Jones');
+INSERT INTO contact (email) VALUES ('jill.doe@email.com');
+INSERT INTO contact (phone_number) VALUES ('555-123-1212');
+INSERT INTO contact (userId) VALUES ('someUser');
 
