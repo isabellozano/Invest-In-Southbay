f79d89d0987f697336ec38fec9ff5b1fcbb45a46
isa <54730392+isabellozano@users.noreply.github.com>
2020-01-28 05:02:28 -0800

    updated search properties js

--- models/index.js
+++ models/index.js
@@ -41,25 +41,25 @@ const sequelize = require("../config/sequelize.js");
 
 const user = {
   all: function(cb) {
-    sequelize.all("users", function(res) {
+    sequelize.all("??", function(res) {
       cb(res);
     });
   },
 
   create: function(cols, vals, cb) {
-    sequelize.create("users", cols, vals, function(res) {
+    sequelize.create("??", cols, vals, function(res) {
       cb(res);
     });
   },
 
   update: function(objColVals, condition, cb) {
-    sequelize.update("users", objColVals, condition, function(res) {
+    sequelize.update("??", objColVals, condition, function(res) {
       cb(res);
     });
   },
   
   delete: function(condition, cb) {
-    sequelize.delete("users", condition, function(res) {
+    sequelize.delete("??", condition, function(res) {
       cb(res);
     });
   }
--- models/search_properties.js
+++ models/search_properties.js
@@ -41,25 +41,25 @@ const sequelize = require("../config/sequelize.js");
 
 const search = {
   all: function(cb) {
-    sequelize.all("searches", function(res) {
+    sequelize.all("search properties", function(res) {
       cb(res);
     });
   },
 
   create: function(cols, vals, cb) {
-    sequelize.create("searches", cols, vals, function(res) {
+    sequelize.create("search properties", cols, vals, function(res) {
       cb(res);
     });
   },
 
   update: function(objColVals, condition, cb) {
-    sequelize.update("searches", objColVals, condition, function(res) {
+    sequelize.update("search properties", objColVals, condition, function(res) {
       cb(res);
     });
   },
   
   delete: function(condition, cb) {
-    sequelize.delete("searches", condition, function(res) {
+    sequelize.delete("search properties", condition, function(res) {
       cb(res);
     });
   }
